02f8f7b645109b236f3d0aa681f74c97b320bcee
mayanhavoc <mayanhavoc@gmail.com>
2023-02-18 21:36:59 -0500

    add layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Header from '$lib/components/Header.svelte';
+    import '$lib/styles/style.scss';
+    import { fade } from 'svelte/transition'
+    export const prerender = true
+    export let data;
+</script>
+    
+
+
+  <Header />
+  {#key data.currentRoute}
+    <main in:fade={{ duration: 150, delay: 150 }} out:fade={{ duration: 150 }}>
+      <slot />
+    </main>
+  {/key}
+  
+  <footer>Hello, I'm the footer.</footer>
