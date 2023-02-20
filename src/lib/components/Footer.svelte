d31370cb531e30c9d4f28aff2598fa75d96aa2e2
mayanhavoc <mayanhavoc@gmail.com>
2023-02-20 18:06:29 -0500

    add Footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,68 @@
+<style>
+    footer {
+        background-color: var(--color-darker);
+        padding: 2em;
+    }
+
+    .footer-heading {
+        color: var(--color-light);
+        font-size: var(--font-lg);
+    }
+
+    .col--footer {
+        font-size: var(--font-xs);
+        color: var(--color-dark);
+    }
+
+    .col--footer-text {
+        font-family: var(--secondary-font);
+    }
+
+    .col--footer-heading {
+        color: var(--color-highlight);
+    }
+
+    .col--footer-list {
+        list-style: none;
+        font-family: var(--secondary-font);
+        padding: 0;
+    }
+</style>
+
+<footer>
+    <div class="container row">
+        <h2 class="footer-heading">just scratching the surface</h2>
+        <div class="footer__content">
+            <div class="col col--footer">
+                <h3 class="col--footer-heading">About our company</h3>
+                <p class="col--footer-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic eveniet cupiditate consequatur ratione nisi ipsum nobis quas accusamus eligendi cumque?</p>
+            </div>
+            <div class="col col--footer">
+                <h3 class="col--footer-heading">Getting around</h3>
+                <ul class="col--footer-list">
+                    <li class="col--footer-item">Home</li>
+                    <li class="col--footer-item">About</li>
+                    <li class="col--footer-item">Contact</li>
+                </ul>
+            </div>
+            <div class="col col--footer">
+                <h3 class="col--footer-heading">Other things</h3>
+                <ul class="col--footer-list">
+                    <li class="col--footer-item">lorem ipsum</li>
+                    <li class="col--footer-item">dolor</li>
+                    <li class="col--footer-item">sit amet</li>
+                    <li class="col--footer-item">consectetur</li>
+                </ul>
+            </div>
+            <div class="col col--footer">
+                <h3 class="col--footer-heading">And more</h3>
+                <ul class="col--footer-list">
+                    <li class="col--footer-item">lorem ipsum</li>
+                    <li class="col--footer-item">dolor</li>
+                    <li class="col--footer-item">sit amet</li>
+                    <li class="col--footer-item">consectetur</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
